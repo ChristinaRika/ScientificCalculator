2a792e1fd4e8129c1bd0d10ad5d67e45a19f6e1a
ChristinaRika <1259094161@qq.com>
2019-03-31 17:40:04 +0800

    Add project files.

---
+++ ScientificCalculator/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace ScientificCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.key1 = new System.Windows.Forms.Button();
+            this.key2 = new System.Windows.Forms.Button();
+            this.key3 = new System.Windows.Forms.Button();
+            this.key4 = new System.Windows.Forms.Button();
+            this.key7 = new System.Windows.Forms.Button();
+            this.key8 = new System.Windows.Forms.Button();
+            this.key5 = new System.Windows.Forms.Button();
+            this.key6 = new System.Windows.Forms.Button();
+            this.key9 = new System.Windows.Forms.Button();
+            this.keyadd = new System.Windows.Forms.Button();
+            this.keysub = new System.Windows.Forms.Button();
+            this.keymul = new System.Windows.Forms.Button();
+            this.keyleft = new System.Windows.Forms.Button();
+            this.key0 = new System.Windows.Forms.Button();
+            this.keyright = new System.Windows.Forms.Button();
+            this.keymod = new System.Windows.Forms.Button();
+            this.keycal = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // key1
+            // 
+            this.key1.Location = new System.Drawing.Point(95, 83);
+            this.key1.Name = "key1";
+            this.key1.Size = new System.Drawing.Size(54, 46);
+            this.key1.TabIndex = 0;
+            this.key1.Text = "1";
+            this.key1.UseVisualStyleBackColor = true;
+            this.key1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // key2
+            // 
+            this.key2.Location = new System.Drawing.Point(197, 83);
+            this.key2.Name = "key2";
+            this.key2.Size = new System.Drawing.Size(53, 46);
+            this.key2.TabIndex = 1;
+            this.key2.Text = "2";
+            this.key2.UseVisualStyleBackColor = true;
+            this.key2.Click += new System.EventHandler(this.key2_Click);
+            // 
+            // key3
+            // 
+            this.key3.Location = new System.Drawing.Point(304, 83);
+            this.key3.Name = "key3";
+            this.key3.Size = new System.Drawing.Size(55, 46);
+            this.key3.TabIndex = 2;
+            this.key3.Text = "3";
+            this.key3.UseVisualStyleBackColor = true;
+            this.key3.Click += new System.EventHandler(this.key3_Click);
+            // 
+            // key4
+            // 
+            this.key4.Location = new System.Drawing.Point(95, 157);
+            this.key4.Name = "key4";
+            this.key4.Size = new System.Drawing.Size(54, 48);
+            this.key4.TabIndex = 3;
+            this.key4.Text = "4";
+            this.key4.UseVisualStyleBackColor = true;
+            this.key4.Click += new System.EventHandler(this.key4_Click);
+            // 
+            // key7
+            // 
+            this.key7.Location = new System.Drawing.Point(95, 227);
+            this.key7.Name = "key7";
+            this.key7.Size = new System.Drawing.Size(54, 44);
+            this.key7.TabIndex = 4;
+            this.key7.Text = "7";
+            this.key7.UseVisualStyleBackColor = true;
+            this.key7.Click += new System.EventHandler(this.key7_Click);
+            // 
+            // key8
+            // 
+            this.key8.Location = new System.Drawing.Point(197, 227);
+            this.key8.Name = "key8";
+            this.key8.Size = new System.Drawing.Size(53, 44);
+            this.key8.TabIndex = 5;
+            this.key8.Text = "8";
+            this.key8.UseVisualStyleBackColor = true;
+            this.key8.Click += new System.EventHandler(this.key8_Click);
+            // 
+            // key5
+            // 
+            this.key5.Location = new System.Drawing.Point(197, 157);
+            this.key5.Name = "key5";
+            this.key5.Size = new System.Drawing.Size(53, 48);
+            this.key5.TabIndex = 6;
+            this.key5.Text = "5";
+            this.key5.UseVisualStyleBackColor = true;
+            this.key5.Click += new System.EventHandler(this.key5_Click);
+            // 
+            // key6
+            // 
+            this.key6.Location = new System.Drawing.Point(304, 157);
+            this.key6.Name = "key6";
+            this.key6.Size = new System.Drawing.Size(55, 48);
+            this.key6.TabIndex = 7;
+            this.key6.Text = "6";
+            this.key6.UseVisualStyleBackColor = true;
+            this.key6.Click += new System.EventHandler(this.key6_Click);
+            // 
+            // key9
+            // 
+            this.key9.Location = new System.Drawing.Point(304, 227);
+            this.key9.Name = "key9";
+            this.key9.Size = new System.Drawing.Size(55, 44);
+            this.key9.TabIndex = 8;
+            this.key9.Text = "9";
+            this.key9.UseVisualStyleBackColor = true;
+            this.key9.Click += new System.EventHandler(this.key9_Click);
+            // 
+            // keyadd
+            // 
+            this.keyadd.Location = new System.Drawing.Point(404, 83);
+            this.keyadd.Name = "keyadd";
+            this.keyadd.Size = new System.Drawing.Size(52, 46);
+            this.keyadd.TabIndex = 9;
+            this.keyadd.Text = "+";
+            this.keyadd.UseVisualStyleBackColor = true;
+            this.keyadd.Click += new System.EventHandler(this.keyadd_Click);
+            // 
+            // keysub
+            // 
+            this.keysub.Location = new System.Drawing.Point(404, 157);
+            this.keysub.Name = "keysub";
+            this.keysub.Size = new System.Drawing.Size(52, 48);
+            this.keysub.TabIndex = 10;
+            this.keysub.Text = "-";
+            this.keysub.UseVisualStyleBackColor = true;
+            this.keysub.Click += new System.EventHandler(this.keysub_Click);
+            // 
+            // keymul
+            // 
+            this.keymul.Location = new System.Drawing.Point(404, 227);
+            this.keymul.Name = "keymul";
+            this.keymul.Size = new System.Drawing.Size(52, 44);
+            this.keymul.TabIndex = 11;
+            this.keymul.Text = "*";
+            this.keymul.UseVisualStyleBackColor = true;
+            this.keymul.Click += new System.EventHandler(this.keymul_Click);
+            // 
+            // keyleft
+            // 
+            this.keyleft.Location = new System.Drawing.Point(95, 297);
+            this.keyleft.Name = "keyleft";
+            this.keyleft.Size = new System.Drawing.Size(54, 43);
+            this.keyleft.TabIndex = 12;
+            this.keyleft.Text = "(";
+            this.keyleft.UseVisualStyleBackColor = true;
+            this.keyleft.Click += new System.EventHandler(this.keyleft_Click);
+            // 
+            // key0
+            // 
+            this.key0.Location = new System.Drawing.Point(197, 297);
+            this.key0.Name = "key0";
+            this.key0.Size = new System.Drawing.Size(53, 43);
+            this.key0.TabIndex = 13;
+            this.key0.Text = "0";
+            this.key0.UseVisualStyleBackColor = true;
+            this.key0.Click += new System.EventHandler(this.key0_Click);
+            // 
+            // keyright
+            // 
+            this.keyright.Location = new System.Drawing.Point(304, 297);
+            this.keyright.Name = "keyright";
+            this.keyright.Size = new System.Drawing.Size(55, 43);
+            this.keyright.TabIndex = 14;
+            this.keyright.Text = ")";
+            this.keyright.UseVisualStyleBackColor = true;
+            this.keyright.Click += new System.EventHandler(this.keyright_Click);
+            // 
+            // keymod
+            // 
+            this.keymod.Location = new System.Drawing.Point(404, 297);
+            this.keymod.Name = "keymod";
+            this.keymod.Size = new System.Drawing.Size(52, 43);
+            this.keymod.TabIndex = 15;
+            this.keymod.Text = "/";
+            this.keymod.UseVisualStyleBackColor = true;
+            this.keymod.Click += new System.EventHandler(this.keymod_Click);
+            // 
+            // keycal
+            // 
+            this.keycal.Location = new System.Drawing.Point(304, 373);
+            this.keycal.Name = "keycal";
+            this.keycal.Size = new System.Drawing.Size(151, 35);
+            this.keycal.TabIndex = 16;
+            this.keycal.Text = "Calculate";
+            this.keycal.UseVisualStyleBackColor = true;
+            this.keycal.Click += new System.EventHandler(this.keycal_Click);
+            // 
+            // Result
+            // 
+            this.Result.FormattingEnabled = true;
+            this.Result.ItemHeight = 15;
+            this.Result.Location = new System.Drawing.Point(557, 83);
+            this.Result.Name = "Result";
+            this.Result.ScrollAlwaysVisible = true;
+            this.Result.Size = new System.Drawing.Size(175, 184);
+            this.Result.TabIndex = 19;
+            this.Result.SelectedIndexChanged += new System.EventHandler(this.Result_SelectedIndexChanged_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(361, 25);
+            this.textBox1.TabIndex = 20;
+            // 
+            // C
+            // 
+            this.C.Location = new System.Drawing.Point(610, 297);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(75, 43);
+            this.C.TabIndex = 21;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.keycal);
+            this.Controls.Add(this.keymod);
+            this.Controls.Add(this.keyright);
+            this.Controls.Add(this.key0);
+            this.Controls.Add(this.keyleft);
+            this.Controls.Add(this.keymul);
+            this.Controls.Add(this.keysub);
+            this.Controls.Add(this.keyadd);
+            this.Controls.Add(this.key9);
+            this.Controls.Add(this.key6);
+            this.Controls.Add(this.key5);
+            this.Controls.Add(this.key8);
+            this.Controls.Add(this.key7);
+            this.Controls.Add(this.key4);
+            this.Controls.Add(this.key3);
+            this.Controls.Add(this.key2);
+            this.Controls.Add(this.key1);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button key1;
+        private System.Windows.Forms.Button key2;
+        private System.Windows.Forms.Button key3;
+        private System.Windows.Forms.Button key4;
+        private System.Windows.Forms.Button key7;
+        private System.Windows.Forms.Button key8;
+        private System.Windows.Forms.Button key5;
+        private System.Windows.Forms.Button key6;
+        private System.Windows.Forms.Button key9;
+        private System.Windows.Forms.Button keyadd;
+        private System.Windows.Forms.Button keysub;
+        private System.Windows.Forms.Button keymul;
+        private System.Windows.Forms.Button keyleft;
+        private System.Windows.Forms.Button key0;
+        private System.Windows.Forms.Button keyright;
+        private System.Windows.Forms.Button keymod;
+        private System.Windows.Forms.Button keycal;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox Result;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button C;
+    }
+}
+
---
+++ ScientificCalculator/Form1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ScientificCalculator
+{
+    public delegate void Tran(char[] a, char[] b, char[] c);
+    public partial class Calculator : Form
+    {
+        public static string exp = "0";
+        public static int top = -1,top1 = -1;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            exp += "1";
+            textBox1.Text += "1";
+        }
+
+        private void key2_Click(object sender, EventArgs e)
+        {
+            exp += "2";
+            textBox1.Text += "2";
+        }
+
+        private void key3_Click(object sender, EventArgs e)
+        {
+            exp += "3";
+            textBox1.Text += "3";
+        }
+
+        private void keyadd_Click(object sender, EventArgs e)
+        {
+            exp += "+";
+            textBox1.Text += "+";
+        }
+
+        private void key4_Click(object sender, EventArgs e)
+        {
+            exp += "4";
+            textBox1.Text += "4";
+        }
+
+        private void key5_Click(object sender, EventArgs e)
+        {
+            exp += "5";
+            textBox1.Text += "5";
+        }
+
+        private void key6_Click(object sender, EventArgs e)
+        {
+            exp += "6";
+            textBox1.Text += "6";
+        }
+
+        private void key7_Click(object sender, EventArgs e)
+        {
+            exp += "7";
+            textBox1.Text += "7";
+        }
+
+        private void key8_Click(object sender, EventArgs e)
+        {
+            exp += "8";
+            textBox1.Text += "8";
+        }
+
+        private void key9_Click(object sender, EventArgs e)
+        {
+            exp += "9";
+            textBox1.Text += "9";
+        }
+
+        private void keyleft_Click(object sender, EventArgs e)
+        {
+            exp += "(";
+            textBox1.Text += "(";
+        }
+
+        private void key0_Click(object sender, EventArgs e)
+        {
+            exp += "0";
+            textBox1.Text += "0";
+        }
+
+        private void keyright_Click(object sender, EventArgs e)
+        {
+            exp += ")";
+            textBox1.Text += ")";
+        }
+
+        private void keymod_Click(object sender, EventArgs e)
+        {
+            exp += "/";
+            textBox1.Text += "/";
+        }
+
+        private void keymul_Click(object sender, EventArgs e)
+        {
+            exp += "*";
+            textBox1.Text += "*";
+        }
+
+        private void keysub_Click(object sender, EventArgs e)
+        {
+            exp += "-";
+            textBox1.Text += "-";
+
+        }
+
+        private void keycal_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            char[] midexp = exp.ToCharArray();
+            char[] sufexp = new char[midexp.Length + 30];
+            char[] stack = new char[sufexp.Length];
+            double[] calstack = new double[30];
+            Tran Trans = new Tran(trans);
+            Trans(midexp, sufexp, stack);
+            Calculate(sufexp, calstack, ref result);
+            Result.Items.Add(string.Format("Result: "+result.ToString()));
+            exp=result.ToString();
+            textBox1.Text = exp;
+        }
+        
+        public static void trans(char[] midexp, char[] sufexp, char[] stack)
+        {
+            int i = 0, j = 0;
+            char e = '0';
+            while (i < midexp.Length)
+            {
+                switch (midexp[i])
+                {
+                    case '(':
+                        Push(stack, midexp[i]);
+                        i++;
+                        break;
+                    case ')':
+                        Pop(stack, ref e);
+                        while (e != '(')
+                        {
+
+                            sufexp[j++] = e;
+                            Pop(stack, ref e);
+                        }
+                        i++;
+                        break;
+                    case '+':
+                    case '-':
+                        while (!Empty(stack))
+                        {
+                            Gettop(stack, ref e);
+                            if (e != '(')
+                            {
+
+                                sufexp[j++] = e;
+                                Pop(stack, ref e);
+                            }
+                            else
+                                break;
+                        }
+                        Push(stack, midexp[i]);
+                        i++;
+                        break;
+                    case '*':
+                    case '/':
+                        while (!Empty(stack))
+                        {
+                            Gettop(stack, ref e);
+                            if (e == '*' || e == '/')
+                            {
+
+                                sufexp[j++] = e;
+                                Pop(stack, ref e);
+                            }
+                            else
+                                break;
+                        }
+                        Push(stack, midexp[i]);
+                        i++;
+                        break;
+                    default:
+                        while (i < midexp.Length && midexp[i] >= '0' && midexp[i] <= '9')
+                        {
+                            sufexp[j++] = midexp[i];
+                            i++;
+                        }
+                        sufexp[j++] = '#';
+                        break;
+                }
+            }
+            while (!Empty(stack))
+            {
+                Pop(stack, ref e);
+                sufexp[j++] = e;
+            }
+            sufexp[j] = '\0';
+
+        }
+        public static void Calculate(char[] sufexp, double[] calstack, ref double result)
+        {
+            double a = 0, b = 0, c;
+            int i = 0;
+            while (sufexp[i] != '\0')
+            {
+                switch (sufexp[i])
+                {
+                    case '+':
+                        Pop1(calstack, ref a);
+                        Pop1(calstack, ref b);
+                        Push1(calstack, b + a);
+                        i++;
+                        break;
+                    case '-':
+                        Pop1(calstack, ref a);
+                        Pop1(calstack, ref b);
+                        Push1(calstack, b - a);
+                        i++;
+                        break;
+                    case '*':
+                        Pop1(calstack, ref a);
+                        Pop1(calstack, ref b);
+                        Push1(calstack, b * a);
+                        i++;
+                        break;
+                    case '/':
+                        Pop1(calstack, ref a);
+                        Pop1(calstack, ref b);
+                        if (a == 0)
+                        {   MessageBox.Show("Error! Try to div 0!");
+                            return;
+                        }
+                        Push1(calstack, b / a);
+                        i++;
+                        break;
+                    default:
+                        c = 0;
+                        while (i < sufexp.Length && sufexp[i] >= '0' && sufexp[i] <= '9')
+                        {
+                            c = (double)(10 * c) + (double)(sufexp[i] - '0');
+                            i++;
+                        }
+                        Push1(calstack, c);
+                        i++;
+                        break;
+                }
+            }
+            Gettop1(calstack, ref result);
+        }
+        public static bool Push(char[] stack, char e)
+        {
+            if (top == stack.Length - 1)
+            {
+                Console.Write("full");
+                return false;
+            }
+            stack[++top] = e;
+            return true;
+        }
+        public static bool Empty(char[] stack)
+        {
+            return top == -1;
+        }
+        public static bool Pop(char[] stack, ref char e)
+        {
+            if (Empty(stack))
+            {
+                Console.Write("Empty");
+                return false;
+            }
+            e = stack[top--];
+            return true;
+        }
+        public static bool Gettop(char[] stack, ref char e)
+        {
+            if (Empty(stack))
+            {
+                Console.Write("Empty");
+                return false;
+            }
+            e = stack[top];
+            return true;
+        }
+        public static bool Push1(double[] stack, double e)
+        {
+            if (top1 == 30 - 1)
+            {
+                Console.Write("full");
+                return false;
+            }
+            stack[++top1] = e;
+            return true;
+        }
+        public static bool Empty1(double[] stack)
+        {
+            return top1 == -1;
+        }
+        public static bool Pop1(double[] stack, ref double e)
+        {
+            if (Empty1(stack))
+            {
+                Console.Write("Empty");
+                return false;
+            }
+            e = stack[top1--];
+            return true;
+        }
+        public static bool Gettop1(double[] stack, ref double e)
+        {
+            if (Empty1(stack))
+            {
+                Console.Write("Empty");
+                return false;
+            }
+            e = stack[top1];
+            return true;
+        }
+
+        private void result_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Result_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CE_Click(object sender, EventArgs e)
+        {
+            top = -1;
+            top1 = -1;
+            textBox1.Text = "";
+            exp = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackgroundImageLayout = ImageLayout.Stretch;
+        }
+    }
+}
